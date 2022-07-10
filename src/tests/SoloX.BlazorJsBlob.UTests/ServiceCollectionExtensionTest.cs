e15258abc841ffa8dc81e53da141e451aa31d65c
Xavier Solau <xaviersolau@yahoo.fr>
2022-07-11 00:22:29 +0200

    Add some tests.

---
+++ src/tests/SoloX.BlazorJsBlob.UTests/ServiceCollectionExtensionTest.cs
@@ -0,0 +1,33 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="ServiceCollectionExtensionTest.cs" company="Xavier Solau">
+// Copyright © 2022 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace SoloX.BlazorJsBlob.UTests
+{
+    public class ServiceCollectionExtensionTest
+    {
+        [Theory]
+        [InlineData(ServiceLifetime.Singleton)]
+        [InlineData(ServiceLifetime.Scoped)]
+        [InlineData(ServiceLifetime.Transient)]
+        public void ItShouldSetupServiceCollectionWithBlogService(ServiceLifetime serviceLifetime)
+        {
+            var servicesMock = new Mock<IServiceCollection>();
+
+            var services = servicesMock.Object;
+
+            services.AddJsBlob(serviceLifetime);
+
+            servicesMock.Verify(x => x.Add(It.Is<ServiceDescriptor>(d => d.Lifetime == serviceLifetime && d.ServiceType == typeof(IBlobService))), Times.Once());
+        }
+
+    }
+}
