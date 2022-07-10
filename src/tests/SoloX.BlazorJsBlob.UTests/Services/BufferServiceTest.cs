d8e464a63cbc6f56ecc8fac1201fa42af7d42f6f
Xavier Solau <xaviersolau@yahoo.fr>
2022-07-10 18:43:59 +0200

    Add some tests.

---
+++ src/tests/SoloX.BlazorJsBlob.UTests/Services/BufferServiceTest.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="BufferServiceTest.cs" company="Xavier Solau">
+// Copyright © 2022 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using FluentAssertions;
+using SoloX.BlazorJsBlob.Services.Impl;
+using Xunit;
+
+namespace SoloX.BlazorJsBlob.UTests.Services
+{
+    public class BufferServiceTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(512)]
+        [InlineData(2048)]
+        [InlineData(16000)]
+        public void ItShouldCreateBufferWithAGivenSize(int size)
+        {
+            var service = new BufferService();
+
+            var buffer = service.Rent(size);
+
+            buffer.Should().NotBeNull();
+            buffer.Length.Should().BeGreaterThanOrEqualTo(size);
+
+            service.Return(buffer);
+        }
+    }
+}
